709c4c8de68a6b886cb16f13c52598399f8eaaed
ilyssa <iwiden3@gmail.com>
2015-10-24 20:54:18 -0700

    jumping

--- game.py
+++ game.py
@@ -18,46 +18,79 @@ def load_image(name, colorkey=None):
 
 def on_ground(panda, platform):
     panda_rect = panda.curr_rect
-    print panda_rect
     tiles = platform.tiles
     for i in range(len(tiles)):
         for j in range(len(tiles[i])):
             if panda_rect.colliderect(tiles[i][j].rect) and tiles[i][j].is_ground:
-                print "on ground"
                 return True
     return False
     
 
 #classes for our game objects
 class Panda(pygame.sprite.Sprite):
-    """moves a clenched fist on the screen, following the mouse"""
     def __init__(self):
         pygame.sprite.Sprite.__init__(self) #call Sprite initializer
         self.image, self.rect = load_image('chimp.bmp', -1)
         self.x = 0
-        self.y = 0
+        self.y = 160
         self.curr_rect = self.rect
+        self.jumping = False
+        self.vely = -10
+        self.max_jump = 200
+        self.go_down = False
 
     def handle_keys(self,platform):
-        """handle keys"""
         key = pygame.key.get_pressed()
-        dist = 10
+        dist = 5
         on_ground(self,platform)
-        if key[K_DOWN]:
-            self.y += dist
-        elif key[K_UP]:
-            self.y -= dist
+        if key[K_SPACE]:
+            self.jumping = True
+            print "jump"
+            self.max_jump = 100
         elif key[K_RIGHT]:
-            self.x += dist
+            if self.x < 390:            
+                self.x += dist
         elif key[K_LEFT]:
-            self.x -= dist
+			if self.x > 0:            
+				self.x -= dist
+        #elif not any(key):
+        if not on_ground(self, platform):
+            self.jumping = True
+        if self.jumping:
+            self.y += self.vely
+            self.vely += 1
+            if on_ground(self,platform):
+                self.jumping = False
+                self.vely = -10
+        
+
+
+     #   if self.jumping:
+        #    print self.y, self.max_jump
+        #    if self.y <= self.max_jump:
+        #        self.go_down = True
+         #       print "1"         
+          #  if self.go_down:
+           #     print "2"
+            #    if on_ground(self,platform):
+             #       self.go_down = False
+              #      self.jumping = False
+               #     print "3"
+               # else:
+                 #   self.y += dist
+                #    print "4"
+           # else:
+            #    self.y -= 10
+        #        print "5"
+
+
         self.curr_rect = Rect(self.x,self.y,self.rect.width,self.rect.height)
         
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
-class Tile:
+class Tile(object):
     def __init__(self, x, y, height, is_ground):
         self.x = x
         self.y = y
@@ -74,7 +107,7 @@ class Tile:
             pygame.draw.rect(surface, sky_color, (self.x, self.y, self.height, self.height)) 
         
         
-class Platform:
+class Platform(object):
     def __init__(self):
         self.ground_color = ( 0, 255, 0)
         self.sky_color = (135, 206, 256)
@@ -105,6 +138,7 @@ def main():
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode((400, 300),0 , 32)
     pygame.display.set_caption('Ilyssa Panda Game!')
+    pygame.key.set_repeat(500,30)
     panda = Panda()
     platform = Platform()
 
@@ -115,6 +149,7 @@ def main():
     DISPLAYSURF.blit(background, (0, 0))
     pygame.display.flip()
     clock = pygame.time.Clock()
+    pygame.time.set_timer(10,60)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
